bc5cc340f0552e7a75f5ef18578953a8b496d0c8
Seth Miller <seth@four43.com>
2016-05-04 22:04:28 -0500

    Removed fat arrow to maintain 0.12 compatibility.

--- test/frameworks/expressjs.js
+++ test/frameworks/expressjs.js
@@ -5,28 +5,28 @@ var assert = require('assert'),
 	request = require('supertest'),
 	sinon = require('sinon');
 
-describe("Express Middleware", function() {
+describe("Express Middleware", function () {
 
 	var app,
 		clock;
 
-	beforeEach(function() {
+	beforeEach(function () {
 		clock = sinon.useFakeTimers(1000);
 
 		app = express();
 		setupExpress(app);
 	});
 
-	afterEach(function() {
+	afterEach(function () {
 		clock.restore();
 	});
 
-	it("should cache basic", function(done) {
+	it("should cache basic", function (done) {
 		request(app)
 			.get('/hello')
 			.expect('Expires', 'Thu, 01 Jan 1970 00:00:31 GMT')
 			.expect(200)
-			.end(function(err, res) {
+			.end(function (err, res) {
 				if (err) throw done(err);
 				// Travel forward in time.
 				clock.tick(10000);
@@ -34,19 +34,19 @@ describe("Express Middleware", function() {
 					.get('/hello')
 					.expect('Expires', 'Thu, 01 Jan 1970 00:00:31 GMT')
 					.expect(200)
-					.end(function(err, res) {
+					.end(function (err, res) {
 						if (err) throw done(err);
 						done();
 					});
 			});
 	});
 
-	it("should be expired", function(done) {
+	it("should be expired", function (done) {
 		request(app)
 			.get('/hello')
 			.expect('Expires', 'Thu, 01 Jan 1970 00:00:31 GMT')
 			.expect(200)
-			.end(function(err, res) {
+			.end(function (err, res) {
 				if (err) throw done(err);
 				// Travel forward in time.
 				clock.tick(40000);
@@ -54,54 +54,54 @@ describe("Express Middleware", function() {
 					.get('/hello')
 					.expect('Expires', 'Thu, 01 Jan 1970 00:01:11 GMT')
 					.expect(200)
-					.end(function(err, res) {
+					.end(function (err, res) {
 						if (err) throw done(err);
 						done();
 					});
 			});
 	});
 
-	it("should be infinity", function(done) {
+	it("should be infinity", function (done) {
 		request(app)
 			.get('/inf')
 			.expect('Expires', 'Sat, 02 Jan 1971 00:00:01 GMT')
 			.expect(200)
-			.end(function(err, res) {
+			.end(function (err, res) {
 				if (err) throw done(err);
 				done();
 			});
 	});
 
-	it("should be respect max size", function(done) {
+	it("should be respect max size", function (done) {
 		async.parallel([
-			(cb) => {
+				function (cb) {
+					request(app)
+						.get('/hello')
+						.expect('Expires', 'Thu, 01 Jan 1970 00:00:31 GMT')
+						.expect(200)
+						.end(cb);
+				},
+				function (cb) {
+					request(app)
+						.get('/world')
+						.expect('Expires', 'Thu, 01 Jan 1970 00:00:11 GMT')
+						.expect(200)
+						.end(cb);
+				}
+			],
+			function (err, results) {
+				if (err) return done(err);
+				// Travel forward in time.
+				clock.tick(10000);
 				request(app)
 					.get('/hello')
-					.expect('Expires', 'Thu, 01 Jan 1970 00:00:31 GMT')
+					.expect('Expires', 'Thu, 01 Jan 1970 00:00:41 GMT')
 					.expect(200)
-					.end(cb);
-			},
-			(cb) => {
-				request(app)
-					.get('/world')
-					.expect('Expires', 'Thu, 01 Jan 1970 00:00:11 GMT')
-					.expect(200)
-					.end(cb);
-			}
-		],
-		function(err, results) {
-			if(err) return done(err);
-			// Travel forward in time.
-			clock.tick(10000);
-			request(app)
-				.get('/hello')
-				.expect('Expires', 'Thu, 01 Jan 1970 00:00:41 GMT')
-				.expect(200)
-				.end(function(err, res) {
-					if(err) return done(err);
-					done();
-				});
-		});
+					.end(function (err, res) {
+						if (err) return done(err);
+						done();
+					});
+			});
 	});
 });
 
@@ -112,17 +112,17 @@ function setupExpress(app) {
 		}
 	}));
 
-	app.get('/hello', function(req, res) {
+	app.get('/hello', function (req, res) {
 		res.set('expires', new Date(Date.now() + 30000).toUTCString());
 		res.send("Hello! " + (new Date).toISOString());
 	});
 
-	app.get('/world', function(req, res) {
+	app.get('/world', function (req, res) {
 		res.set('expires', new Date(Date.now() + 10000).toUTCString());
 		res.send("World! " + (new Date).toISOString());
 	});
 
-	app.get('/inf', function(req, res) {
+	app.get('/inf', function (req, res) {
 		res.set('expires', Infinity);
 		res.send("World! " + (new Date).toISOString());
 	});
